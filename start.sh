b002f265188ae4e7d483365202ca426e79249782
pmg1989 <972401854@qq.com>
2018-03-25 14:55:57 +0800

    fix docker env

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ docker rm next-redux-immutable-todo
 #docker build -t next/redux-immutable-todo .
 docker run --name next-redux-immutable-todo \
 -d -p 3000:3000 \
--v /Users/newband/Documents/demo/nextjs/nextjs-redux-immutable-todo/.next:/usr/src/app/.next \
+-v "$PWD"/.next:/usr/src/app/.next \
 next/redux-immutable-todo
