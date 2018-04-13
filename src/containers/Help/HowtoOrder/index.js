2f7eaa7868ba5f2cef9b5038389f0ee1d4823401
pmg1989 <972401854@qq.com>
2018-04-13 16:12:26 +0800

    fix env

--- src/containers/Help/HowtoOrder/index.js
+++ src/containers/Help/HowtoOrder/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Container, Header, TabBar  } from 'components'
+import { Container, Header, TabBar } from 'components'
 import Content from './Content'
 
 const HowtoOrder = () => {
