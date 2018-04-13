589d0b5c7203c4e6c5e761dc72eee25c1634b480
pmg1989 <972401854@qq.com>
2018-04-13 22:38:03 +0800

    fix rem.js

--- static/js/rem.js
+++ static/js/rem.js
@@ -4,8 +4,8 @@
         recalc = function() {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
-            if (clientWidth > 768) {
-              clientWidth = 768;
+            if (clientWidth > 375) {
+              clientWidth = 375;
             }
             // docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
             docEl.style.fontSize = 50 * (clientWidth / 375) + 'px';
