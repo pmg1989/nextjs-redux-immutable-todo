e4ddc86f5e15aa6b420d5be6cefd406e4374c6eb
pmg1989 <972401854@qq.com>
2018-04-14 14:33:05 +0800

    fix env

--- src/containers/ListDetail.js
+++ src/containers/ListDetail.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Head, Nav } from 'components'
 
-const ListDetail = () => {
+const ListDetail = ({ query }) => {
   return (
     <div className="viewport">
       <Head title="list-detail" />
       <Nav />
       <div className="box">
-        i am list detail
+        i am list detail: id: {query.id}
       </div>
       <div>Loading data from {process.env.BACKEND_URL}</div>
     </div>
   )
+}
+
+ListDetail.propTypes = {
+  query: PropTypes.object.isRequired,
+}
+
+ListDetail.getInitialProps = ({ query }) => {
+  return {
+    query,
+  }
 }
 
 export default ListDetail
